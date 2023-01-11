04083c2211195055baa17bb0dc8173649d08c331
Maxp73 <massimo_sposato@hotmail.com>
2023-01-11 09:44:59 +0000

    first commit
    first commit

---
+++ src/main/java/democitybank/logic/BusinessLogic.java
@@ -0,0 +1,12 @@
+package democitybank.logic;
+
+public class BusinessLogic {
+	public int[] secondHighest = null;
+
+	public BusinessLogic(int[] secondHighest) {
+		super();
+		this.secondHighest = secondHighest;
+	}
+	
+ 
+}
